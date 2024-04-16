ef865bde378fbf03e8d54999a4d9db1ff94d48e4
Oskar Zdziarski <oskarzdz@gmail.com>
2024-04-16 14:35:37 -0700

    update test script

--- test.sh
+++ test.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
 
-OUTPUT=$(cat test.txt | ./build/bin/warmup2 2> /dev/null)
-echo $OUTPUT | diff -s test_key.txt -
+EXECUTABLE="./build/bin/warmup2"
+if [ ! -f $EXECUTABLE ]; then
+    echo "EXECUTABLE \"$EXECUTABLE\" CANNOT BE FOUND"
+    exit 1
+fi
+
+OUTPUT=$(cat test.txt | $EXECUTABLE 2> /dev/null)
+echo "$OUTPUT" | diff -s test_key.txt -
